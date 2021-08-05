9474a9b943102971084933aa35b9386df1c4505f
dnggngn825 <53506074+dnggngn825@users.noreply.github.com>
2021-08-05 21:10:29 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+function success(pos) {
+    var crd = pos.coords;
+    var lat = crd.latitude;
+    var lon = crd.longitude;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+
+    // Create object to fetch data
+    let weather = {
+        city: "Melbourne",
+        API_KEY:"09e89a7a425148c3b39cf28839f1ab8f",
+        fetchData: function() {
+            fetch("https://api.openweathermap.org/data/2.5/weather?lat="+
+            lat+
+            "&lon="+
+            lon+
+            "&units=metric"+
+            "&appid="+ 
+            this.API_KEY)
+            .then((res) => res.json())
+            .then((data) => {
+                let title = document.getElementById("title-text");
+                let temp = document.getElementById("temp-text")
+                let feel = document.getElementById("feel-like-text");
+                let weatherDes = document.getElementById("weather-description");
+                let humid = document.getElementById("humidity-text");
+                let wind = document.getElementById("wind-speed-text");
+                let word = data.weather[0].description;
+                let min = document.getElementById("min-temp-text");
+                let max = document.getElementById("max-temp-text");
+                
+                title.innerHTML = "Weather in " + data.name;
+                temp.innerHTML = Math.round(data.main.temp) + '\xB0C';
+                feel.innerHTML = "Feel like " + Math.round(data.main.feels_like) + '\xB0C';
+                weatherDes.innerHTML = word[0].toUpperCase() + word.slice(1).toLowerCase();
+                humid.innerHTML = "Humidity: " + data.main.humidity + "%";
+                wind.innerHTML = "Wind speed: " + data.wind.speed + " km/h";
+                min.innerHTML = Math.round(data.main.temp_min) ;
+                max.innerHTML = Math.round(data.main.temp_max) ;
+
+                console.log("fetch data")
+            })
+        }
+    }
+
+    weather.fetchData()
+
+}
+  
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+const loadData = async () => {
+    await navigator.geolocation.getCurrentPosition(success, error, options)
+}
+
+setInterval(loadData,3000);
+  
+
+
+
+
+
+
